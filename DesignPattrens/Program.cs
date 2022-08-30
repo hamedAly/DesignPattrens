3913cabcbdf8216465def1f688f80e5a5de38076
Hamed aly <h.aly@nt-me.com>
2022-08-30 20:49:18 +0200

    add solution to github

---
+++ DesignPattrens/Program.cs
@@ -0,0 +1,15 @@
+﻿n using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattrens
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
