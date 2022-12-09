2cdaaddf38b9a12dbc7466e50c1fc2d591d6b720
hamed aly <7amed.aly@gmail.com>
2022-12-09 18:54:37 +0200

    command pattern

--- DesignPattrens/Program.cs
+++ DesignPattrens/Program.cs
@@ -6,14 +6,63 @@ using System.Threading.Tasks;
 
 namespace DesignPattrens
 {
+    // Invoker 
     internal class Program
     {
+        private CommandExcute _commandExcure;
+        public Program()
+        {
+
+        }
         static void Main(string[] args)
         {
 
-            Shape circle = new Circle();
-            new CircleDecorator(circle).Draw();
+
             Console.ReadKey();
         }
     }
+    // ref
+    public interface Command
+    {
+        void Excute();
+    }
+    // Sender 
+    class CoptyTextReceiver : Command
+    {
+        private TextEditor _text;
+        public CoptyTextReceiver(TextEditor text)
+        {
+            this._text = text;
+        }
+        public void Excute()
+        {
+            _text.Coptytext();
+        }
+    }
+    class PasteTextReceiver : Command
+    {
+        private TextEditor _text;
+        public PasteTextReceiver(TextEditor text)
+        {
+            this._text = text;
+        }
+        public void Excute()
+        {
+            _text.PasteText();
+        }
+    }
+    // Receiver
+    class TextEditor
+    {
+        public void Coptytext()
+        {
+            //some text here
+        }
+        public void PasteText()
+        {
+            // some text her
+        }
+    }
+
 }
+
